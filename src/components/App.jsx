2fe4dd1f3c3a1fc4c32862cc0e84a017592045fd
tiramisa <miller19988@gmail.com>
2023-08-10 19:39:54 +0100

    contact app list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#aaaaaa',
-        fontFamily: 'FontAwesome',
-      }}
-    >
-    </div>
-  );
-};
+import { Component } from "react";
+  
+import ContactForm from "components/contactList/ContactList";
+  
+export default class App extends Component{
+  state={
+  contacts:[],
+  }
+
+  handleAddContact = (newContact) =>
+    this.setState(({ contacts }) => ({
+    contacts:[...contacts,newContact]
+    }))
+  handleGetUniqueContacts = (name) => {
+    const {contacts} = this.state;
+    const isExistContact = !!contacts.find(contact => contact.name === name)
+    isExistContact && alert('Контакт уже существует')
+    return !isExistContact
+     }
+  render() {
+    return <>
+      <h2>Form Contact</h2>
+      <ContactForm onAdd={this.handleAddContact } />
+    </>
+  }
+}
---
+++ src/components/contactForm/ContactForm.jsx
@@ -0,0 +1,10 @@
+  import { Component } from "react";
+import ContactForm from "components/contactList/ContactList";
+export default class App extends Component{
+  render() {
+    return <>
+      <h2>Form Contact</h2>
+      <ContactForm/>
+    </>
+  }
+}
