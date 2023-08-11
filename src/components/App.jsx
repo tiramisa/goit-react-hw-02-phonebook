d8c53f5ce1c063a6dc4d27f8db48d916bf01d00f
tiramisa <miller19988@gmail.com>
2023-08-11 21:54:42 +0100

    filter app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import ContactForm from "./contactForm/ContactForm";
 import { ContactsList } from "./contactsList/ContactsList";
+import Filter from "./filter/Filter";
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
   handleAddContact = (newContact) => {
@@ -32,15 +39,33 @@ export class App extends Component {
     return !isExistContact;
   };
 
+  handleFilterChange = (value) => {
+    this.setState({ filter: value });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
   render() {
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+
     return (
       <>
         <h2>Form Contact</h2>
         <ContactForm onAdd={this.handleAddContact} getUniqueContacts={this.handleGetUniqueContacts} />
-        <ContactsList contacts={this.state.contacts} onRemove={this.handleRemoveContact} />
+        <h2>Contact List</h2>
+        <Filter filter={filter} onChange={this.handleFilterChange} />
+        <ContactsList contacts={visibleContacts} onRemove={this.handleRemoveContact} />
       </>
     );
   }
 }
+
+
 
 
---
+++ src/components/filter/Filter.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Filter = ({ filter, onChange }) => {
+  return (
+    <input
+      type="text"
+      name="filter"
+      value={filter}
+      onChange={({ target }) => onChange(target.value)}
+      placeholder="Enter name for Search"
+    />
+  );
+};
+
+export default Filter;
+
